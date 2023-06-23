d34a6119c19c76693e2c57d1f515ed71885f00c0
Olubodun <bolowys33@gmail.com>
2023-06-23 19:06:50 +0100

    Add variables to task_two.js

--- tasks/task_two.js
+++ tasks/task_two.js
@@ -1,8 +1,14 @@
 const num1 = 10
 const num2 = 5
 
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
-console.log(num1 % num2)
+const resultAdd = num1 + num2
+const resultSubtract = num1 - num2
+const resultMultiply = num1 * num2
+const resultDivide = num1 / num2
+const resultModulo = num1 % num2
+
+console.log(resultAdd)
+console.log(resultSubtract)
+console.log(resultMultiply)
+console.log(resultDivide)
+console.log(resultModulo)
